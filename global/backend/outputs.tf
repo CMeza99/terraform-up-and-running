4cb5dab13481799d051c1f40b861516c922e00a9
Carlos Meza <carlos@digitalr00ts.com>
2019-02-07 14:26:48 -0800

    output - add names

--- global/backend/outputs.tf
+++ global/backend/outputs.tf
@@ -1,5 +1,13 @@
+output "s3_bucket_name" {
+  value = "${aws_s3_bucket.terraform_state.id}"
+}
+
 output "s3_bucket_arn" {
   value = "${aws_s3_bucket.terraform_state.arn}"
+}
+
+output "dynamodb_table_name" {
+  value = "${aws_dynamodb_table.terraform_state_lock.id}"
 }
 
 output "dynamodb_table_arn" {
