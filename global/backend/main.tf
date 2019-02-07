dacec635522175e6dea4d0d4b233aa29cebce549
Carlos Meza <carlos@digitalr00ts.com>
2019-02-07 14:07:27 -0800

    uncomment lifecycle

--- global/backend/main.tf
+++ global/backend/main.tf
@@ -18,11 +18,9 @@ resource "aws_s3_bucket" "terraform_state" {
     Name = "Terraform State"
   }
 
-/*
   lifecycle {
     prevent_destroy = true
   }
-*/
 }
 
 resource "aws_dynamodb_table" "terraform_state_lock" {
@@ -40,9 +38,7 @@ resource "aws_dynamodb_table" "terraform_state_lock" {
     Name = "Terraform State Lock Table"
   }
 
-/*
   lifecycle {
     prevent_destroy = true
   }
-*/
 }
